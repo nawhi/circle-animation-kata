5286acc4a396c130a77ff908c080ef656a041e17
nawhi <25394965+nawhi@users.noreply.github.com>
2024-09-17 13:40:44 +0100

    Enable Vite's error overlay for runtime as well as compile errors

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,18 @@ import store from './store/store';
 import App from './components/app';
 import './index.css';
 
+// This enables Vite's error overlay for runtime errors as well as static ones
+// credit: https://github.com/vitejs/vite/issues/2076#issuecomment-1500765186
+if (import.meta.env.DEV) {
+  window.onerror = (...args) => {
+    const [, , , , err] = args;
+    const ErrorOverlay = customElements.get('vite-error-overlay');
+    if (ErrorOverlay) {
+      document.body.appendChild(new ErrorOverlay(err));
+    }
+  };
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
